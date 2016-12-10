2bf33745bc9a494df74c9de0d43b3197f80b4d9a
Vihan B <contact@vihan.org>
2016-12-09 19:22:40 -0800

    Initial commit
    Signed-off-by: Vihan B <contact@vihan.org>

---
+++ src/index.js
@@ -0,0 +1,114 @@
+export default function (babel) {
+  const { types: t } = babel;
+  
+  return {
+    visitor: {
+      FunctionDeclaration(path) {
+        var name = path.node.id.name;
+
+        if (name === "_classCallCheck" || name === "_possibleConstructorReturn")
+          path.remove();
+        else if (name === "_inherits")
+          path.node.body = t.blockStatement([
+            t.expressionStatement(
+              t.assignmentExpression(
+                "=",
+                t.memberExpression(
+                  t.Identifier("subClass"),
+                  t.Identifier("prototype")
+                ),
+                t.callExpression(
+                  t.memberExpression(
+                    t.Identifier("Object"),
+                    t.Identifier("create")
+                  ),
+                  [
+                    t.logicalExpression(
+                      "&&",
+                      t.Identifier("superClass"),
+                      t.memberExpression(
+                        t.Identifier("superClass"),
+                        t.Identifier("prototype")
+                      )
+                    ),
+                    
+                    t.objectExpression([
+                      t.objectProperty(
+                        t.Identifier("constructor"),
+                        t.objectExpression([
+                          t.objectProperty(
+                            t.Identifier("value"),
+                            t.Identifier("subClass")
+                          ),
+                          t.objectProperty(
+                            t.Identifier("enumerable"),
+                            t.booleanLiteral(false)
+                          ),
+                          t.objectProperty(
+                            t.Identifier("writable"),
+                            t.booleanLiteral(false)
+                          ),
+                          t.objectProperty(
+                            t.Identifier("configurable"),
+                            t.booleanLiteral(false)
+                          )
+                        ])
+                      )
+                    ])
+                  ]
+              	)
+              )
+            ),
+            t.expressionStatement(
+              t.conditionalExpression(
+                t.logicalExpression(
+                  "&&",
+                  t.Identifier("superClass"),
+                  t.memberExpression(
+                    t.Identifier("Object"),
+                    t.Identifier("setPrototypeOf")
+                  )
+                ),
+                t.callExpression(
+                  t.memberExpression(
+                    t.Identifier("Object"),
+                    t.Identifier("setPrototypeOf")
+                  ),
+                  [
+                    t.Identifier("subClass"),
+                    t.Identifier("superClass")
+                  ]
+                ),
+                t.assignmentExpression(
+                  "=",
+                  t.memberExpression(
+                    t.Identifier("subClass"),
+                    t.Identifier("__proto__")
+                  ),
+                  t.Identifier("superClass")
+                )
+              ),
+            )
+          ]);
+      },
+      
+      ExpressionStatement(path) {
+        if (t.isCallExpression(path.node.expression)) {
+          var name = path.node.expression.callee.name;
+          if (name === "_classCallCheck")
+            path.remove();
+        }
+      },
+      
+      ReturnStatement(path) {
+        if (t.isCallExpression(path.node.argument)) {
+          var name = path.node.argument.callee.name;
+          if (name === "_possibleConstructorReturn")
+            path.node.argument = t.Identifier("this");
+        }
+      }
+    }
+  };
+}
+
+
